d16103fb9a520e08e8556eed010b89a4f316f14e
Scorpio <diallomikey13@yahoo.fr>
2019-01-30 14:07:37 -0500

    added component file structure wip

---
+++ src/components/speciesOverall/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import speciesInfo from "../speciesInfo";
+
+class speciesOverall extends Component {
+render() {
+    return (
+      <div>
+          <speciesInfo />
+      </div>
+    )
+};
+}
+export default speciesOverall;
