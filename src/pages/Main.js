ba8af82101b88f5fff1a1ff90324493cf11475f6
Jesse Schimmel <jhowardschimmel@gmail.com>
2019-02-02 11:36:52 -0500

    fixed multiple jsx error in SpeciesInfo fixed unsed var error for SpeciesOverall render fixed Main.js error for unread Link component and removed MainButton import in Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../App.css';
-import MainButtons from "../components/MainButtons";
+
 
 
 function Main() {
     return (
-      <MainButtons />
+      <Link />
+      
+
     );
 }
 
