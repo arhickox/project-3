3468d26bfa7f7306c8f16e6f7a1faa685ce0531d
Shahein Moussavi <shahein.moussavi@turner.com>
2019-02-07 16:55:48 -0500

    Basic structure for creating views

--- client/src/pages/View.js
+++ client/src/pages/View.js
@@ -1,9 +1,29 @@
 import React from "react";
+import API from "../utils/API";
 
-function View() {
+class View extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      characters: []
+    }
+  }
+
+  componentDidMount() {
+    API.getCharacters().then(res => {
+      this.setState({ characters: res.data });
+      console.log(this.state.characters);
+    });
+  }
+
+  render() {
+    const characters = this.state.characters.map((character, key) => <div key={key} value={character.id}>{character.fullname}</div>);
     return (
-        <h1>View Page</h1>
-    );
+    <div>
+      <h1>View Characters</h1>
+      {characters}
+    </div>
+    )
+  }
 }
-
 export default View;
